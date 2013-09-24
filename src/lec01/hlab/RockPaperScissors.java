3b8f64e55e7ea3cd82a8f0280f927834759275ef
Adam Gerber <adam.gerber@yahoo.com>
2013-09-24 18:13:22 -0500

    added some more classes to lecture01

---
+++ src/lec01/hlab/RockPaperScissors.java
@@ -0,0 +1,66 @@
+package lec01.hlab;
+
+/**
+ 7) Remember the childhood game “Rock, Paper, Scissors”? It is a two-player game in which each person simultaneously
+ chooses either rock, paper, or scissors. Rock beats scissors but loses to paper, paper beats rock but loses to scissors,
+ and scissors beats paper but loses to rock. The following code prompts player 1 and player 2 to each enter a
+ string: rock, paper, or scissors. Finish the code by adding nested if statements to appropriately report “Player 1
+ wins”, “Player 2 wins”, or “It is a tie.”
+ */
+import java.util.Scanner;
+public class RockPaperScissors
+{
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Player 1: Choose rock, scissors, or paper:");
+        String player1 = scan.next().toLowerCase();
+        System.out.println("Player 2: Choose rock, scissors, or paper:");
+        String player2 = scan.next().toLowerCase();
+        if (player1.equals("rock"))
+        {
+            if (player2.equals("rock"))
+            {
+                System.out.println("It is a tie.");
+            }
+            if (player2.equals("scissors"))
+            {
+                System.out.println("Player 1 wins.");
+            }
+            if (player2.equals("paper"))
+            {
+                System.out.println("Player 2 wins.");
+            }
+        }
+        else if (player1.equals("scissors"))
+        {
+            if (player2.equals("rock"))
+            {
+                System.out.println("Player 2 wins.");
+            }
+            if (player2.equals("scissors"))
+            {
+                System.out.println("It is a tie.");
+            }
+            if (player2.equals("paper"))
+            {
+                System.out.println("Player 1 wins.");
+            }
+        }
+        else if (player1.equals("paper"))
+        {
+            if (player2.equals("rock"))
+            {
+                System.out.println("Player 1 wins.");
+            }
+            if (player2.equals("scissors"))
+            {
+                System.out.println("Player 2 wins.");
+            }
+            if (player2.equals("paper"))
+            {
+                System.out.println("It is a tie.");
+            }
+        }
+    }
+}
---
+++ src/lec02/glab/CollisionDetect.java
@@ -0,0 +1,97 @@
+package lec02.glab;
+
+
+import java.awt.*;
+import java.util.Scanner;
+
+/**
+Given two circles in the 1st quad of a cartesian 2D plane, determine if they are colliding or not.
+ */
+public class CollisionDetect {
+
+    public static void main(String[] args) {
+        Circle cirOne = null;
+        Circle cirTwo = null;
+
+
+        //determine the x delta and y delta
+        //determine size of hypotenuse
+        //if hypot < (rad1 + rad2)
+            //no collision
+        //else
+            //collision
+
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Type the center point coordinates and radius of your first circle separated by commas e.g. 5,12,3.5: ");
+        String[] strSplits1 = scan.nextLine().split(",");
+
+        System.out.println("Type the center point coordinates and radius of your second circle separated by commas e.g. 5,12,3.5: ");
+        String[] strSplits2 = scan.nextLine().split(",");
+
+
+        try {
+            for (int nC = 0; nC <strSplits1.length ; nC++) {
+                cirOne = new Circle(
+                        Integer.parseInt(strSplits1[0]),
+                        Integer.parseInt(strSplits1[1]),
+                        Double.parseDouble(strSplits1[2])
+                );
+            }
+
+            for (int nC = 0; nC <strSplits2.length ; nC++) {
+                cirTwo = new Circle(
+                        Integer.parseInt(strSplits2[0]),
+                        Integer.parseInt(strSplits2[1]),
+                        Double.parseDouble(strSplits2[2])
+                );
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid input " + e.getMessage());
+            return;
+        }
+
+
+        double dDeltaX = Math.abs(cirOne.getCenter().getX() -  cirTwo.getCenter().getX() );
+        double dDeltaY = Math.abs(cirOne.getCenter().getX() -  cirTwo.getCenter().getX() );
+        double dHypot = Math.sqrt((dDeltaX * dDeltaX ) + (dDeltaY * dDeltaY));
+        if (dHypot < cirOne.getRad() + cirTwo.getRad()){
+            System.out.println("No collision");
+        } else {
+            System.out.println("Collision");
+        }
+    }
+
+
+    private static class Circle {
+        private Point mCenter;
+        private double mRad;
+
+        private Circle(Point center, double rad) {
+            mCenter = center;
+            mRad = rad;
+        }
+
+        private Circle(int nX, int nY, double rad) {
+            mCenter = new Point(nX, nY);
+            mRad = rad;
+        }
+
+        private Point getCenter() {
+            return mCenter;
+        }
+
+        private void setCenter(Point center) {
+            mCenter = center;
+        }
+
+        private double getRad() {
+            return mRad;
+        }
+
+        private void setRad(double rad) {
+            mRad = rad;
+        }
+    }
+
+}
