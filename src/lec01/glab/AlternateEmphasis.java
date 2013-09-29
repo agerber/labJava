c0b4abc9d128d4032b5917d9a90632cab7fcfac5
Adam Gerber <adam.gerber@yahoo.com>
2013-09-29 16:34:15 -0500

    comment reset master head

--- src/lec01/glab/AlternateEmphasis.java
+++ src/lec01/glab/AlternateEmphasis.java
@@ -9,6 +9,7 @@ public class AlternateEmphasis {
      Write a program that asks the user for a sentence and then splits the sentence into words, and then
      displays alternating words as UPPERCASE.
 
+
      Write the same program, but don't split the setence, rather detect a space and flip a boolean flag
      */
 	public static void main(String[] args) {
