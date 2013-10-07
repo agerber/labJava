66c031e2674ef43d20c82719b3f49f350848f25a
Adam Gerber <adam.gerber@yahoo.com>
2013-10-07 16:04:22 -0500

    Revert "Revert "cleared-out todays lab classes""
    This reverts commit 912b8a35e03f08cbafe30d39e80636688b01c50f.

--- src/lec02/glab/RandomDriver.java
+++ src/lec02/glab/RandomDriver.java
@@ -15,17 +15,6 @@ public class RandomDriver {
 
 
     public static void main(String[] args) {
-        Random random = new Random();
-        //will generate a number between 0-0.999999999
-        System.out.println(random.nextDouble());
-
-        //will multiply the generated double 0-0.9999) by the param
-        //then truncate it (not round)
-        // 99.999999 will be truncated to 99
-        // 0.99999 will be truncated to 0
-        //will generate a number between 0 and 99
-        System.out.println(random.nextInt(100));
-
 
 
     }
