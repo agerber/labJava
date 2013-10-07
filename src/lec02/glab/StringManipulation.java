b60a0152bf645de5508ddf0d1b992f13216f76d5
Adam Gerber <adam.gerber@yahoo.com>
2013-10-07 14:57:19 -0500

    changed stringDriver and added RandomDriver

--- src/lec02/glab/StringManipulation.java
+++ src/lec02/glab/StringManipulation.java
@@ -1,5 +1,7 @@
 package lec02.glab;
 
+import java.awt.*;
+
 public class StringManipulation {
 
 	/**
@@ -42,10 +44,49 @@ public class StringManipulation {
 		System.out.println(nPos);
 		
 		System.out.println(strState.endsWith("i"));
-		
-		
-		
-	}
+
+
+        //string pools
+
+        String strBig0 = new String("Illinois");
+        String strBig1 = new String("Indiana");
+        String strBig2 = new String("Iowa");
+        String strBig3 = new String("Michigan");
+        String strBig4 = new String("Michigan State");
+        String strBig5 = new String("Minnesota");
+        String strBig6 = new String("Nebraska");
+        String strBig7 = new String("Northwestern");
+        String strBig8 = new String("Ohio State");
+        String strBig9 = "Pennsylvania";
+        String strBig10 = "Purdue";
+        String strBig11 = "Wisconsin";
+
+        String strNoLake0 = new String("Iowa");
+        String strNoLake1 = new String("Nebraska");
+
+
+        System.out.println(56.21 == 56.21);
+
+        //the == sign is comparing memory address
+        Rectangle recOne = new Rectangle(2,3,4,5);
+        Rectangle recTwo = new Rectangle(2,3,4,5);
+
+        //this is false
+        System.out.println(recOne == recTwo);
+
+
+        recOne = recTwo;
+        //this is true
+        System.out.println(recOne == recTwo);
+
+
+        //this should be false, but sometimes it's true??
+        System.out.println(strNoLake1 == strBig6);
+
+
+
+
+    }
 	
 	
 	
