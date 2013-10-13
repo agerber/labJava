fbb3f5d8b4a024bdf65fdcc36a82d64ba06b311c
Adam Gerber <adam.gerber@yahoo.com>
2013-10-13 17:51:51 -0500

    fixed posterMaker

--- src/lec03/glab/poster/PosterMaker.java
+++ src/lec03/glab/poster/PosterMaker.java
@@ -14,14 +14,12 @@ public class PosterMaker {
 		
 		PosterElement posE = 
 				new PosterElement(
-				  "file:\\\\\\C:\\dev\\lec03\\src\\edu\\uchicago\\cs\\java\\lec03\\resources\\pos_obama.jpg");
-
+				  "//src//lec03//glab//resources//pos_lennon.jpg");
 
 		System.out.println(posE.display());
-		
-		
+
 		Banner banBottom = new Banner(
-				"file:\\\\\\C:\\dev\\lec03\\src\\edu\\uchicago\\cs\\java\\lec03\\resources\\pos_banner.jpg",
+                "//src//lec03//glab//resources//pos_banner.jpg",
 				PosterElement.ZOO_DIM + 4, 20);
 	
 		System.out.println(banBottom.display());
