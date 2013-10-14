0b5de975adab8e5077b71fbf82609960a917af4e
Adam Gerber <adam.gerber@yahoo.com>
2013-10-14 13:26:57 -0500

    polymorphism

--- src/lec03/glab/simplepoly/PolySimple.java
+++ src/lec03/glab/simplepoly/PolySimple.java
@@ -8,10 +8,17 @@ public class PolySimple {
 	/**
 	 * @param args
 	 */
+
+    //Cntrl-H for class hierarchy on PC
 	public static void main(String[] args) {
 		
 		
 		//very simple polymorphism
+        //first rule of polymorphism:
+            //you can store a subclass object in a superclass reference
+        //second rule of polymorphism
+            //you can store an implementing object in an interface reference
+
 	
 		Object[] objObjects = new Object[6];
 		
@@ -22,8 +29,9 @@ public class PolySimple {
 		objObjects[4] = new Rectangle(5,8,9,12);
 		objObjects[5] = new MyClass();
 		
-		for (Object obj : objObjects) 
+		for (Object obj : objObjects){
 			System.out.println(obj.getClass() + " : " + obj.toString());
+        }
 				
 		System.out.println("-----------------------------------");
 		
@@ -37,9 +45,7 @@ public class PolySimple {
 
 		//an example using instanceof to determine the the type of the object reference
 		ArrayList<Number> numTemps = new ArrayList<Number>();
-		
-		
-		
+
 		numTemps.add(new Double(121.6));
 		numTemps.add(new Integer(95));
 		numTemps.add(new Integer(219));
