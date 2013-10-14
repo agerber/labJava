3b79269ebee9b37130553227d55a81762260b73c
Adam Gerber <adam.gerber@yahoo.com>
2013-10-14 17:50:14 -0500

    cleared out lab

--- src/lec03/glab/employee/Student.java
+++ src/lec03/glab/employee/Student.java
@@ -1,21 +1,7 @@
 package lec03.glab.employee;
 
-public class Student extends Person {
+public class Student  {
 
 	
-	private String mMajor;
-	
-	
-	public Student(String name, String major) {
-		super(name);
-		mMajor = major;
-	}
-
-
-	@Override
-	public String getDescription() {
-		// TODO Auto-generated method stub
-		return "a Student majoring in " + mMajor;
-	}
 
 }
