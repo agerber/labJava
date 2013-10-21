53d7e823742e7546a71ac7de35995b094ab0b78e
Adam Gerber <adam.gerber@yahoo.com>
2013-10-21 10:57:12 -0500

    varargs refactor

--- src/lec04/glab/varargs/VarArgs.java
+++ src/lec04/glab/varargs/VarArgs.java
@@ -1,26 +1,32 @@
 package lec04.glab.varargs;
 
-import java.lang.String;import java.lang.System;public class Elipses {
+import java.lang.String;import java.lang.System;public class VarArgs {
 
 
 	public static void main(String[] args) {
-
 
 		sum("The sum of",8,2,5,7, 6, 8, 101, -9);
 
 	}
 	
-	//elipses aka varargs, introduced in Java5
+	//varargs, introduced in Java5
+    //notice how the varargs argument is last!
+    //once inside the method, the param Type... is treated as Type[]
+    //so, int... becomes int[]
 	private static void sum(String strSum, int... nParams){
 		int nResult = 0;
 		System.out.print(strSum + " ");
 		//notice that the int... is converted to int[] inside the method
 		for (int nC = 0; nC < nParams.length; nC++) {
-			if (nC == nParams.length - 1)
+            //just output to the command-line
+			if (nC == nParams.length - 1){
 				System.out.print("and " + nParams[nC]);
-			else
+            }
+			else {
 			     System.out.print(nParams[nC] + ", ");
-			
+            }
+
+            //do the summing
 			nResult += nParams[nC]; 
 		}
 		
