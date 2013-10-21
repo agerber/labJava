145305e7d60c72aa3eaf77d0ce6a3a7dbdfaac87
Adam Gerber <adam.gerber@yahoo.com>
2013-10-21 10:38:18 -0500

    employee example refactored

--- src/lec04/glab/employee/Employee.java
+++ src/lec04/glab/employee/Employee.java
@@ -9,75 +9,36 @@ import java.util.GregorianCalendar;
 
 public class Employee extends Person {
 
-	
 	private double mSalary;
 	private Date mHireDate;
-	
-	
-	
-	
-	
+
 	public Employee(String strName, double dSalary, int nYear, int nMonth, int nDay) {
 		super(strName);
 		this.mSalary = dSalary;
-		GregorianCalendar greHire = new GregorianCalendar(nYear, nMonth -1, nDay);
-		this.mHireDate = greHire.getTime();
+		this.mHireDate = new GregorianCalendar(nYear, nMonth -1, nDay).getTime();
 		
 	}
-	
-	
-	
-
-
-
-
 
 	public double getSalary() {
 		return mSalary;
 	}
 
-
-
-
-
-
-
-
 	public void setSalary(double salary) {
 		mSalary = salary;
 	}
-
-
-
-
-
-
-
 
 	public Date getHireDate() {
 		return mHireDate;
 	}
 
-
-
-
-
-
-
-
 	public void setHireDate(Date hireDate) {
 		this.mHireDate = hireDate;
 	}
-
-
-
-
 
 	public void raiseSalary(double dPercent){
 		double dRaise = mSalary * dPercent / 100;
 		mSalary += dRaise;
 	}
-
 
 
 	@Override
--- src/lec04/glab/employee/Student.java
+++ src/lec04/glab/employee/Student.java
@@ -2,6 +2,7 @@ package lec04.glab.employee;
 
 
 
+//student is slighlty more complex and it's concrete
 public class Student extends Person {
 
 	
