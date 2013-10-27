9dd404b885979652cd74d5694858b2470c0ca9b9
Adam Gerber <adam.gerber@yahoo.com>
2013-10-27 18:10:13 -0500

    killed recursion, tuned-up reflection

--- src/lec05/glab/reflection/ReflectionTest.java
+++ src/lec05/glab/reflection/ReflectionTest.java
@@ -5,130 +5,115 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.util.Scanner;
+import java.util.Stack;
 
 
-//found here: http://www.hackchina.com/en/r/18861/ReflectionTest.java__html
+//originally found here: http://www.hackchina.com/en/r/18861/ReflectionTest.java__html  and grossly modified
 //using ReflectionTest and Horstmann as search params
 
-public class ReflectionTest
-{  
-   public static void main(String[] args)
-   {  
-      // read class name from command line args or user input
-      String name;
-      if (args.length > 0) 
-         name = args[0];
-      else 
-      {
-         Scanner in = new Scanner(System.in);
-         System.out.println("Enter class name (e.g. java.util.Date): ");
-         name = in.next();
-      }
+public class ReflectionTest {
+    public static void main(String[] args) {
+        // read class name from command line args or user input
+        String strClass = "";
+        Scanner scan = new Scanner(System.in);
+        Class cls = null;
+        while (true) {
+            try {
+                System.out.println("Enter \"exit\" to quit, or a fully qualified class name (e.g. java.util.Date): ");
+                strClass = scan.next();
+                if (strClass.equalsIgnoreCase("exit")){
+                    System.exit(0);
+                }
+                //the following line could throw an exception
+                cls = Class.forName(strClass);
 
-      try
-      {  
-         // print class name and superclass name (if != Object)
-         
-    	  //instantiate the class and superclass ojbects 
-    	  Class cl = Class.forName(name);
-         Class supercl = cl.getSuperclass();
-         
-         //first line
-         System.out.print("class " + name);
-         if (supercl != null && supercl != Object.class)
-            System.out.print(" extends " + supercl.getName());
+                //create a stack and push the original class onto it
+                Stack<Class> clsStacks = new Stack<>();
+                clsStacks.push(cls);
 
-         System.out.print("\n{\n");
-         System.out.println("//FIELDS");
-         printFields(cl);
-         System.out.println();
-         System.out.println("//CONSTRUCTORS");
-         printConstructors(cl);
-         System.out.println();
-         System.out.println("//METHODS");
-         printMethods(cl);
-         System.out.println();
- 
-         System.out.println("}");
-      }
-      catch(ClassNotFoundException e) { e.printStackTrace(); }
-      System.exit(0);
-   }
+                //keep pushing its superclass until superclass is null (I've reached grand-daddy Object)
+                while (clsStacks.peek().getSuperclass() != null) {
+                    clsStacks.push(clsStacks.peek().getSuperclass());
+                }
 
-   /**
-      Prints all constructors of a class
-      @param cl a class
-    */
-   public static void printConstructors(Class cl)
-   {  
-      Constructor[] constructors = cl.getDeclaredConstructors();
+                while (!clsStacks.empty()) {
+                    cls = clsStacks.pop();
+                    System.out.println("###############CLASS###############\n" +cls.toString() + "\n###############CLASS###############\n");
+                    System.out.println("//FIELDS");
+                    printFields(cls);
+                    System.out.println();
+                    System.out.println("//CONSTRUCTORS");
+                    printConstructors(cls);
+                    System.out.println();
+                    System.out.println("//METHODS");
+                    printMethods(cls);
+                    System.out.println();
 
-      for (Constructor c : constructors)
-      {           
-         String name = c.getName();
-         System.out.print("   " + Modifier.toString(c.getModifiers()));
-         System.out.print(" " + name + "(");
+                }
 
-         // print parameter types
-         Class[] paramTypes = c.getParameterTypes();
-         for (int j = 0; j < paramTypes.length; j++)
-         {  
-            if (j > 0) System.out.print(", ");
-            System.out.print(paramTypes[j].getName());
-         }
-         System.out.println("); //no-arg constructor");
-      }
-   }
+            } catch (ClassNotFoundException e) {
+               // e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                System.out.println("That's now a valid class name, try again.");
+                continue;
+            }
+        }
 
-   /**
-      Prints all methods of a class
-      @param cl a class
-    */
-   public static void printMethods(Class cl)
-   {  
-      Method[] methods = cl.getDeclaredMethods();
+    }
 
-      for (Method m : methods)
-      {  
-         Class retType = m.getReturnType();
-         String name = m.getName();
+    public static void printConstructors(Class cl) {
+        Constructor[] constructors = cl.getDeclaredConstructors();
 
-         // print modifiers, return type and method name
-         System.out.print("   " + Modifier.toString(m.getModifiers()));
-         System.out.print(" " + retType.getName() + " " + name + "(");
+        for (Constructor c : constructors) {
+            String name = c.getName();
+            System.out.print("   " + Modifier.toString(c.getModifiers()));
+            System.out.print(" " + name + "(");
 
-         // print parameter types
-         Class[] paramTypes = m.getParameterTypes();
-         for (int j = 0; j < paramTypes.length; j++)
-         {  
-            if (j > 0) System.out.print(", ");
-            System.out.print(paramTypes[j].getName());
-         }
-         System.out.println(");");
-      }
-   }
+            // print parameter types
+            Class[] paramTypes = c.getParameterTypes();
+            for (int j = 0; j < paramTypes.length; j++) {
+                if (j > 0) System.out.print(", ");
+                System.out.print(paramTypes[j].getName());
+            }
+            System.out.println("); //no-arg constructor");
+        }
+    }
 
-   /**
-      Prints all fields of a class
-      @param cl a class
-    */
-   public static void printFields(Class cl)
-   {  
-      Field[] fields = cl.getDeclaredFields();
 
-  
-      
-      for (Field f : fields)
-      {  
-    	  //let's check for final
-    	  String strMods = Modifier.toString(f.getModifiers());
-    	  Class type = f.getType();
-          String name = f.getName();
+    public static void printMethods(Class cl) {
+        Method[] methods = cl.getDeclaredMethods();
 
-		  System.out.print("   " + Modifier.toString(f.getModifiers()));
-	      System.out.println(" " + type.getName() + " " + name + ";");
+        for (Method m : methods) {
+            Class retType = m.getReturnType();
+            String name = m.getName();
 
-         
-      }
-   }
+            // print modifiers, return type and method name
+            System.out.print("   " + Modifier.toString(m.getModifiers()));
+            System.out.print(" " + retType.getName() + " " + name + "(");
+
+            // print parameter types
+            Class[] paramTypes = m.getParameterTypes();
+            for (int j = 0; j < paramTypes.length; j++) {
+                if (j > 0) System.out.print(", ");
+                System.out.print(paramTypes[j].getName());
+            }
+            System.out.println(");");
+        }
+    }
+
+    public static void printFields(Class cl) {
+        Field[] fields = cl.getDeclaredFields();
+
+
+        for (Field f : fields) {
+
+            String strMods = Modifier.toString(f.getModifiers());
+            Class type = f.getType();
+            String name = f.getName();
+
+            System.out.print("   " + Modifier.toString(f.getModifiers()));
+            System.out.println(" " + type.getName() + " " + name + ";");
+
+
+        }
+    }
 }
