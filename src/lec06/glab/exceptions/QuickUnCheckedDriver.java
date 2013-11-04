c5aa3f16f0f259887276c56d7c9a95935036f22e
Adam Gerber <adam.gerber@yahoo.com>
2013-11-04 17:29:38 -0600

    cleared out for lab

--- src/lec06/glab/exceptions/QuickUnCheckedDriver.java
+++ src/lec06/glab/exceptions/QuickUnCheckedDriver.java
@@ -12,49 +12,6 @@ import java.util.Scanner;
  */
 public class QuickUnCheckedDriver {
 
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-
-
-        while(true){
-            System.out.println("Please type a valid integer or exit to quit:");
-            String strValue = scan.next();
-            if(strValue.equalsIgnoreCase("exit")){
-                break;
-            }
-            try {
-                System.out.println("Echo: "  + getUserInteger(strValue));
-            } catch (BadUserDataException e) {
-                System.out.println(e.getMessage() + " That's not valid input, try again");
-                continue;
-            }
-            System.out.println("Thank you, let's try again.");
-
-        }
-        System.out.println("game over");
-
-
-    }
-
-    /**
-     *
-     * @param str
-     * @return
-     * @throws BadUserDataException
-     * @pre str must be a valid integer representation
-     */
-    public static int getUserInteger(String str) throws  BadUserDataException {
-        int nReturn = -1;
-        try {
-            nReturn = Integer.parseInt(str);
-        } catch (Exception e) {
-            throw new BadUserDataException(e);
-        }
-
-        return nReturn;
-
-    }
-
 
 
 
