c4a29c449c48af50e4425b09d4fa9f26e4ab7a15
Adam Gerber <adam.gerber@yahoo.com>
2013-11-04 17:29:55 -0600

    Revert "cleared out for lab"
    This reverts commit c5aa3f16f0f259887276c56d7c9a95935036f22e.

--- src/lec06/glab/debugger/ConditionalDriver.java
+++ src/lec06/glab/debugger/ConditionalDriver.java
@@ -2,7 +2,32 @@ package lec06.glab.debugger;
 
 public class ConditionalDriver {
 
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		String[] strTeams = {"Bears", "Browns", "Vikings", "Colts", "Jets"};
+		
+		printTeams(strTeams);
 
+	}
+	
+	
+	private static void printTeams(String[] strParams){
+		
+		//conditional breakpoints
+        System.out.println("goodbye");
+
+		for (int nC = 0; nC < strParams.length; nC++) {
+			System.out.println(strParams[nC]);
+		}
+		
+		System.out.println("goodbye");
+		
+		
+	}
+	
 	
 
 }
--- src/lec06/glab/debugger/RecursiveDriver.java
+++ src/lec06/glab/debugger/RecursiveDriver.java
@@ -2,6 +2,30 @@ package lec06.glab.debugger;
 
 public class RecursiveDriver {
 
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	
+		//macdonalds translator
+		String str = "mmm mmmm good, I love mocha during my morning commute.";
+		String strAd = replaceRec(str, 'm', ' ');
+		System.out.println(strAd);
+	 
 
+	}
+	
+	
+	// ===============================================
+	// ==a char replace function using recursion
+	// ===============================================
+	public static String replaceRec(String str, char cOld, char cNew) {
+		if (str.length() == 0)
+			return "";
+		else if (str.charAt(0) == cOld)
+			return cNew + replaceRec(str.substring(1), cOld, cNew);
+		else
+			return str.charAt(0) + replaceRec(str.substring(1), cOld, cNew);
+	}
 
 }
