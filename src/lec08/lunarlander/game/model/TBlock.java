48ee9710ca9466110693534518acff210727b71c
Adam Gerber <adam.gerber@yahoo.com>
2013-11-15 14:06:19 -0600

    constr and getters/setters

--- src/lec08/lunarlander/game/model/TBlock.java
+++ src/lec08/lunarlander/game/model/TBlock.java
@@ -11,4 +11,38 @@ public class TBlock {
     private int mHeight;
     private boolean mLanding;
 
+
+    //constructor
+    public TBlock(int height, boolean landing, int width) {
+        mHeight = height;
+        mLanding = landing;
+        mWidth = width;
+    }
+
+
+
+    //getters and setters
+    public int getHeight() {
+        return mHeight;
+    }
+
+    public void setHeight(int height) {
+        mHeight = height;
+    }
+
+    public boolean isLanding() {
+        return mLanding;
+    }
+
+    public void setLanding(boolean landing) {
+        mLanding = landing;
+    }
+
+    public int getWidth() {
+        return mWidth;
+    }
+
+    public void setWidth(int width) {
+        mWidth = width;
+    }
 }
