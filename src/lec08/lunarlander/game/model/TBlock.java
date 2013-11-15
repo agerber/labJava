c57b652c1416510f5b0249ec7c3298711a541e55
Adam Gerber <adam.gerber@yahoo.com>
2013-11-15 14:20:12 -0600

    more refactoring with draw and colors

--- src/lec08/lunarlander/game/model/TBlock.java
+++ src/lec08/lunarlander/game/model/TBlock.java
@@ -24,6 +24,7 @@ public class TBlock {
 
     }
 
+    // we can use this for both collision detection and drawing.
     public Rectangle getBounds(){
         return new Rectangle(mOrigin, new Dimension((int)mOrigin.getY(), sWidth));
     }
@@ -31,6 +32,12 @@ public class TBlock {
     //we're going to need a draw method so that we can draw this block the the graphics context of the double-buffered
     //image
     public void draw(Graphics g){
+        if (isLanding()){
+            g.setColor(Color.CYAN);
+        } else {
+            g.setColor(Color.RED);
+        }
+        g.drawRect(mOrigin.x, mOrigin.y, getBounds().x, getBounds().y);
 
     }
 
