1017a27e6231773ebe2874661b8801fcf613f1d8
Adam Gerber <adam.gerber@yahoo.com>
2014-10-20 12:15:05 -0500

    weight metric

--- src/lec04/glab/construct_me/Driver.java
+++ src/lec04/glab/construct_me/Driver.java
@@ -13,17 +13,23 @@ public class Driver {
     public static void main(String[] args) {
 
         ArrayList<Person> perPeeps = new ArrayList<>();
-        perPeeps.add(new Person());
-        perPeeps.add(new Person());
-        perPeeps.add(new Person());
+        perPeeps.add(new Person("Dan"));
+        perPeeps.add(new Person("Jason"));
+        perPeeps.add(new Person("Liz"));
         perPeeps.add(new Person("Charlie"));
+        perPeeps.add(new Person("Charlie", 150));
+        perPeeps.add(new Person("Charlie", 160));
+        perPeeps.add(new Person("Charlie", 160));
 
         for (Person perPeep : perPeeps) {
             System.out.println(perPeep.toString());
         }
 
-        System.out.println(perPeeps.get(0).equals(perPeeps.get(2)));
+        System.out.println(perPeeps.get(6).equals(perPeeps.get(5)));
         System.out.println(perPeeps.get(0).equals(perPeeps.get(3)));
+
+        Person perDan = perPeeps.get(0);
+        System.out.println(perDan.getName() + "'s wieght in kilos is " + Metric.lbsToKilos(perDan.getWeight()));
 
 
 
