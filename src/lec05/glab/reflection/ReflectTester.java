c33a784a96ce4c78683f8327d7c0d3059f270fac
Adam Gerber <adam.gerber@yahoo.com>
2014-10-27 11:43:34 -0500

    reflector test

---
+++ src/lec05/glab/reflection/ReflectTester.java
@@ -0,0 +1,38 @@
+package lec05.glab.reflection;
+
+import java.awt.*;
+import java.awt.geom.Rectangle2D;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * Created by ag on 10/27/2014.
+ */
+public class ReflectTester {
+
+
+
+    //store an HashMap in a Map Interface
+
+    public static void main(String[] args) {
+
+        //store a concrete class in an abstract super-class reference
+        Rectangle2D r2dRec = new Rectangle(1,2,3,4);
+        Reflector.printClass(r2dRec.getClass());
+
+        //store a concreate class object in an implemented interface reference
+        Map<String, String> map = new HashMap<>();
+        map.put("Jason", "CS");
+        map.put("Liz", "Econ");
+        map.put("Dan", "Physics");
+        Reflector.printClass(map.getClass());
+
+        //store a concrete class in an concrete super-class reference
+        Object obj = (Object) r2dRec;
+        Reflector.printClass(obj.getClass());
+
+
+
+    }
+}
