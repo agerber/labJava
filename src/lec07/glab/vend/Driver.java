19c02fcf17ef019b6a33d2f626b13752885392ac
Adam Gerber <adam.gerber@yahoo.com>
2015-03-27 15:19:26 -0500

    cherry-picked the answers

--- src/lec07/glab/vend/Driver.java
+++ src/lec07/glab/vend/Driver.java
@@ -31,12 +31,66 @@ public class Driver {
         vndMachine.stockMe(prdProducts);
 
 
+        outer:
+        while (true) {
 
+            System.out.println("Type 'i' for insert coins or 's' for select product or type 'exit' to quit");
+            String strInitial = sScanner.nextLine();
+            strInitial = strInitial.toUpperCase();
+            String strMoney, strSelect;
+            switch (strInitial){
+                case "I":
+                  strMoney = addMoney();
+                    vndMachine.insertCoins(strMoney);
+                    System.out.println("Credits : " + vndMachine.howMuchInserted());
+                  break;
+                case "S":
+                    System.out.println(vndMachine.showSelection());
+                    strSelect = makeSelection();
+                    if (strSelect.equalsIgnoreCase("AABBCC")){
+                        ArrayList<Coin> conCashOuts = vndMachine.cashOut();
+                        System.out.println("Jackpot!");
+                        for (Coin conCashOut : conCashOuts) {
+                            System.out.println(conCashOut);
+                        }
+                        break;
+                    }
+                    Product prdProduct = vndMachine.vend(strSelect);
+                    if (prdProduct != null)
+                        System.out.println("Thank you and Enjoy: " + prdProduct);
+                    else {
+                        System.out.print("You inserted " + vndMachine.howMuchInserted() + " : Insufficient coins or out of stock");
+                        ArrayList<Coin> conReturns = vndMachine.returnCoins();
+                        System.out.println(", here is your money back: ");
+                        for (Coin conReturn : conReturns) {
+                            System.out.println(conReturn);
+                        }
+
+                    }
+                  break;
+                case "EXIT":
+                    break outer;
+                default:
+                  break;
+            }
+
+        }
+        System.out.println("Thank you for vending");
 
     }
 
+    private static String addMoney(){
 
+        System.out.println("Insert Coins like so: q q d d n");
+        return sScanner.nextLine().toUpperCase();
 
+    }
+
+    private static String makeSelection(){
+        System.out.println("Please choose a product such as 'A2'");
+        return sScanner.nextLine().toUpperCase();
+
+    }
 
 
 }
