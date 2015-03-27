05edd0c0052e232e33cb9cd44c996b20cba94943
Adam Gerber <adam.gerber@yahoo.com>
2015-03-27 15:14:48 -0500

    Revert "Revert "cleared out code""
    This reverts commit 362abdbd24ed4b1f0b6a7c87f0f873101aadb4bc.

--- src/lec06/glab/abstract_me/InterfaceDriver.java
+++ src/lec06/glab/abstract_me/InterfaceDriver.java
@@ -10,15 +10,7 @@ public class InterfaceDriver {
 
 
     public static void main(String[] args) {
-        Runnable runner = new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("hello from runner, my type is: " + this.getClass().getName());
-            }
-        };
 
-        Thread thread = new Thread(runner);
-        thread.start();
 
     }
 
