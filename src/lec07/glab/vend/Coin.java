6eb61a9240dd7f4a0bb1f9350e506759d2544b8c
Adam Gerber <adam.gerber@yahoo.com>
2015-03-27 15:19:34 -0500

    Revert "Revert "cleared out code""
    This reverts commit 3367387bf4c756fb9e5a8a5faa353dd0eb26f18f.

--- src/lec07/glab/vend/Coin.java
+++ src/lec07/glab/vend/Coin.java
@@ -14,44 +14,5 @@ public class Coin {
 
     private static DecimalFormat sDecimalFormat = new DecimalFormat("$0.00");
 
-    public Coin(double value) {
-        mValue = value;
-    }
 
-    public Coin(String  strVal){
-
-        strVal = strVal.toUpperCase();
-        double dVal = 0.0;
-
-         switch (strVal){
-             case "Q":
-                 dVal = 0.25;
-                 break;
-             case "D":
-                 dVal = 0.10;
-                 break;
-             case "N":
-                 dVal = 0.05;
-                 break;
-             default:
-                 dVal = 0.00;
-                 break;
-
-         }
-
-        setValue(dVal);
-    }
-
-    public double getValue() {
-        return mValue;
-    }
-
-    public void setValue(double value) {
-        mValue = value;
-    }
-
-    @Override
-    public String toString(){
-        return sDecimalFormat.format(getValue());
-    }
 }
