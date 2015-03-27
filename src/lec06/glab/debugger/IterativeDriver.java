69b5179c36e0041e8cdd11955369237904abe4ce
Adam Gerber <adam.gerber@yahoo.com>
2015-03-27 15:13:57 -0500

    cleared out code

--- src/lec06/glab/debugger/IterativeDriver.java
+++ src/lec06/glab/debugger/IterativeDriver.java
@@ -9,33 +9,11 @@ public class IterativeDriver {
 	public static void main(String[] args) {
 
 
-		//first day of class
-		MyDate mdt = new MyDate(29,8,2014);
-
-		MyDate mdtEndQtr = new MyDate(8,11,2014);
-
-		while(mdt.compareTo(mdtEndQtr) <= 0){
-
-            System.out.println(mdt);
-			addDays(7, mdt);
-
-		
-		}
 
 
 	}
 
-    /**
-     *
-     * @param nAdd
-     * @param mdt
-     * @should add days to MyDate
-     */
-	private static void addDays(int nAdd, MyDate mdt){
-		for (int nD = 0; nD < nAdd; nD++) {
-			mdt.addDay();
-		}
-	}
+
 	
 
 
