515c7379526f01c8add360883c831075ce6eb7c3
Adam Gerber <adam.gerber@yahoo.com>
2015-03-28 10:03:03 -0500

    Revert "Revert "cleared out blackjack""
    This reverts commit f47babc18e6344a6d275c1edbdcdaaf9c9bcdf84.

--- src/lec08/glab/reflection/ReflectDriver.java
+++ src/lec08/glab/reflection/ReflectDriver.java
@@ -12,17 +12,7 @@ import java.awt.*;
 public class ReflectDriver {
 
     public static void main(String[] args) {
-//        Rectangle recMe = new Rectangle(5,6,45,89);
-//        Reflector.printClass(recMe.getClass());
-//
-//        String strMe = "Adam";
-//        Reflector.printClass(strMe.getClass());
 
-        try {
-            Reflector.printClass("java.lang.Double");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        }
     }
 
 }
