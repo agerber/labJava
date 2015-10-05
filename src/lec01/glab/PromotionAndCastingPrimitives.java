135298fe412fdd0f603035ea2e5b7fabffd3d875
Adam Gerber <gerber@uchicago.edu>
2015-10-04 20:58:00 -0500

    more downcasting examples

--- src/lec01/glab/PromotionAndCastingPrimitives.java
+++ src/lec01/glab/PromotionAndCastingPrimitives.java
@@ -14,15 +14,23 @@ public class PromotionAndCastingPrimitives {
 		//promotion works when:
 		//there are two operands of different primtive types; the smaller precision operand will get promoted to larger precision operand
 		double dResult = dOperand1 / nOperand2; //here nOperand2 is promoted to double
-		//the primitive operand on the left of the assignment operator is of greater precision than the result of the right expression
+
+		//integer division - will truncate, not round!
+		int nResultIntDivision =  23/ 8;
+
+
+
+		//the primitive result on the left of the assignment operator is of greater precision than the result of the right expression
 		//in this case, the result of integer division is 2.66666 trucated to 2, then the result is assigned as (2.0)
 		float fResult = nOperand2 / 3;
+
 		//you concatenate a string with a primitive, the primitive is promoted to a string
 		String strResult = "Hello" + dOperand1;
-		
-		System.out.println(dResult);
-		System.out.println(fResult);
-		System.out.println(strResult);
+
+		System.out.println("nResultIntDivision: " +nResultIntDivision);
+		System.out.println("dResult: " +dResult);
+		System.out.println("fResult: " +fResult);
+		System.out.println("strResult: " +strResult);
 		
 		//you can downcast a primitive. 
 		//upcasting happens automatically; it's called promotion and you lose no precision
