709af6e42ca6925c0180f642b32d21a362c8fc5a
Adam Gerber <gerber@uchicago.edu>
2016-04-03 15:18:43 -0500

    fixed more downcasting

--- src/lec01/glab/MoreDownCasting.java
+++ src/lec01/glab/MoreDownCasting.java
@@ -17,26 +17,26 @@ public class MoreDownCasting {
         System.out.println("float values");
 
         //double to float downcasting
-        double lValue1 = 0.000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000012;
-        double lValue2 = 101.6546465465464321351654313465123168768546134650646546546546546545645645646545646546546546545646546654564654646546540303212;
-        float fValue1 = (float)lValue1;
-        float fValue2 = (float)lValue2;
+        double dValue1 = 0.000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000012;
+        double dValue2 = 101.6546465465464321351654313465123168768546134650646546546546546545645645646545646546546546545646546654564654646546540303212;
+        float fValue1 = (float)dValue1;
+        float fValue2 = (float)dValue2;
 
 
-        System.out.println(String.valueOf(lValue1));
-        System.out.println(String.valueOf(fValue1));
+        System.out.println("As float " + String.valueOf(fValue1));
+        System.out.println("As double " + String.valueOf(dValue1));
+
+        System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&&&&");
+
+        System.out.println("As float " + String.valueOf(fValue2));
+        System.out.println("As double " + String.valueOf(dValue2));
 
 
-
-        System.out.println(String.valueOf(fValue2));
-        System.out.println(String.valueOf(lValue2));
-
-
-        System.out.println("trucate and then round");
+        System.out.println("truncate and then round");
         //truncate by casting to int
-        System.out.println((int)  lValue2);
+        System.out.println((int)  dValue2);
         //round by rounding
-        System.out.println(Math.round(lValue2));
+        System.out.println(Math.round(dValue2));
 
 
 
