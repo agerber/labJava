8975920994e322bb3806aa1202189f939833f19d
Adam Gerber <=adam.gerber@yahoo.com>
2016-04-10 17:24:36 -0500

    this from static context

--- src/lec03/glab/employee/Executive.java
+++ src/lec03/glab/employee/Executive.java
@@ -2,12 +2,12 @@ package lec03.glab.employee;
 
 public final class Executive extends Manager {
 
-	
-	
+
+
 	private double mStockValue;
 
 	public Executive(String name, double salary, int nYear, int nMonth,
-			int nDay, double bonus, double stockValue) {
+					 int nDay, double bonus, double stockValue) {
 		super(name, salary, nYear, nMonth, nDay, bonus);
 		mStockValue = stockValue;
 	}
@@ -19,11 +19,18 @@ public final class Executive extends Manager {
 	public void setStockValue(double stockValue) {
 		mStockValue = stockValue;
 	}
-	
+
 	@Override
 	public String getDescription() {
 		// TODO Auto-generated method stub
-		return String.format("an exec with a salary of " +  getSalary()+ " and a bonus of " + getBonus() + " and a stock comp of " + getStockValue());
+		return String.format("an exec with a salary of " +  getSalary()+ " and a bonus of " + getBonus() +
+				" and a stock comp of " + getStockValue()) +
+
+				getAlphaOrder(this);
 	}
-	
+
+	private static String getAlphaOrder(Executive executive){
+		return  "MMMMMMMMMMMM".compareTo(executive.getName()) > 1 ? "name greater than M" : "name less than M";
+	}
+
 }
