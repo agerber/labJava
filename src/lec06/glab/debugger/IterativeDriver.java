92fc450ed135a0b28a422b42b51d0ade9ee0264a
Adam Gerber <=adam.gerber@yahoo.com>
2016-05-02 11:08:13 -0500

    advance days in debugger

--- src/lec06/glab/debugger/IterativeDriver.java
+++ src/lec06/glab/debugger/IterativeDriver.java
@@ -9,15 +9,16 @@ public class IterativeDriver {
 	public static void main(String[] args) {
 
 
-		//first day of class
-		MyDate mdt = new MyDate(29,8,2014);
+		//first day of class march 28th
+		MyDate mdt = new MyDate(28,2,2016);
 
-		MyDate mdtEndQtr = new MyDate(8,11,2014);
+		//last day of class June 6th
+		MyDate mdtEndQtr = new MyDate(6,5,2016);
 
 		while(mdt.compareTo(mdtEndQtr) <= 0){
 
             System.out.println(mdt);
-			addDays(7, mdt);
+			advancedDays(7, mdt);
 
 		
 		}
@@ -31,7 +32,7 @@ public class IterativeDriver {
      * @param mdt
      * @should add days to MyDate
      */
-	private static void addDays(int nAdd, MyDate mdt){
+	private static void advancedDays(int nAdd, MyDate mdt){
 		for (int nD = 0; nD < nAdd; nD++) {
 			mdt.addDay();
 		}
