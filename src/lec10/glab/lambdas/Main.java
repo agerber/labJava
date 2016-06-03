51acb53d01d9aeb5beaacef50f8f0be081e9f907
Adam Gerber <=adam.gerber@yahoo.com>
2016-06-03 14:21:59 -0500

    more lambdas

--- src/lec10/glab/lambdas/Main.java
+++ src/lec10/glab/lambdas/Main.java
@@ -4,13 +4,18 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.IntPredicate;
+import java.util.function.LongPredicate;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
 
 
 public class Main {
 
 
-    public static void main(String... args){
+    public static void main(String... args) {
 
         ArrayList<Circle> theseCircles = new ArrayList<Circle>();
 
@@ -23,52 +28,156 @@ public class Main {
         theseCircles.add(new Circle(20, 25, 20, "AntiBob"));
         theseCircles.add(new Circle(425, 440, 40, "AntiBatman"));
 
-//        Java 7
-        System.out.println("Java 7 Results: ");
-//        Clone
-        ArrayList<Circle> j7Circles = new ArrayList<Circle>();
-        for (Circle c: theseCircles){
-            try {
-                j7Circles.add(c.clone());
-            } catch (CloneNotSupportedException cnse) {
-                System.out.println(cnse.getStackTrace());
-            }
+////        Java 7
+//        System.out.println("Java 7 Results: ");
+////        Clone
+//        ArrayList<Circle> j7Circles = new ArrayList<Circle>();
+//        for (Circle c: theseCircles){
+//            try {
+//                j7Circles.add(c.clone());
+//            } catch (CloneNotSupportedException cnse) {
+//                System.out.println(cnse.getStackTrace());
+//            }
+//
+//        }
 
-        }
 
-//        Sort (2 Possible Ways)
-//        Collections.sort(j7Circles, new Java7Comparator());
+        //Behavior Parameterization
 
-        Collections.sort(j7Circles, new Comparator<Circle>() {
+        Collections.sort(theseCircles, new Comparator<Circle>() {
             @Override
             public int compare(Circle o1, Circle o2) {
                 return Double.compare(o1.getArea(), o2.getArea());
             }
         });
 
-//        Print Results
-        for (Circle c : j7Circles){
-            System.out.println(c.toString());
+
+        Collections.sort(theseCircles, new Java7Comparator());
+
+        Collections.sort(theseCircles, (Circle o1, Circle o2) -> Double.compare(o1.getArea(), o2.getArea()));
+        Collections.sort(theseCircles, (Circle o1, Circle o2) -> o1.getName().compareTo(o2.getName()));
+
+
+        //Create a stream and filter it.
+        System.out.println("FILTER");
+        theseCircles
+                .stream()
+                .filter(c -> c.getName().contains("Anti"))
+                .forEach(c -> System.out.println(c));
+
+
+        System.out.println();
+        System.out.println("MAP");
+        theseCircles
+                .stream()
+                .map(c -> c.toString().toUpperCase())
+                .forEach(s -> System.out.println(s));
+
+
+        System.out.println();
+        System.out.println("FILTER+MAP");
+        theseCircles
+                .stream()
+                .filter(c -> c.getName().contains("Anti"))
+                .map(c -> c.toString().toUpperCase())
+                .forEach(s -> System.out.println(s));
+
+
+
+        System.out.println();
+        System.out.println("FILTER+MAP+COUNTER");
+
+        AtomicInteger atomicInteger = new AtomicInteger(0);
+        theseCircles
+                .stream()
+                .filter(c -> c.getName().contains("Anti"))
+                .map(s -> s.toString().toUpperCase())
+                .sorted()
+                .forEach(
+                     s ->
+                     {
+                         System.out.println(s);
+                         atomicInteger.getAndIncrement();
+                     }
+                );
+
+        System.out.println("number of s " + atomicInteger);
+
+
+
+
+        //Create parraellel stream
+
+        List<Integer> intVals = new ArrayList<>();
+
+        for (int i = 0; i < Short.MAX_VALUE ; i++) {
+            intVals.add(i);
         }
 
+        //will not respect the sort as this is done in parellel.
+
+        intVals
+                .parallelStream()
+                .filter(l -> isPrime(l))
+
+                //this will kill your parellelism
+               // .forEachOrdered(l -> System.out.println(l));
+                .forEach(l -> System.out.println(l));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //Consume
+
+
+
+//        for (Circle circle : theseCircles) {
+//            System.out.println(circle);
+//        }
+
+        theseCircles.forEach(c -> System.out.println(c.getArea()));
+
+        theseCircles.forEach(System.out::println);
+
+        //filter, map, reduce
+
+
+
+
+
 //        Java 8
-        System.out.println("\n\nJava 8 Results: ");
-//        Clone
-        ArrayList<Circle> j8Circles = new ArrayList<Circle>();
+//        System.out.println("\n\nJava 8 Results: ");
+////        Clone
+//        ArrayList<Circle> j8Circles = new ArrayList<Circle>();
 
 //        In theory (line will not compile!)
-//        theseCircles.forEach(c -> j8Circles.add(c.clone));
+       // theseCircles.forEach(c -> j8Circles.add(c.clone()));
 
-//        Practical
-        theseCircles.forEach( c -> {
-            try {
-                j8Circles.add(c.clone());
-            } catch (CloneNotSupportedException cnse) {
-                System.out.println(cnse.getStackTrace());
-            }
-        });
+////        Practical
+//        theseCircles.forEach( c -> {
+//            try {
+//                j8Circles.add(c.clone());
+//            } catch (CloneNotSupportedException cnse) {
+//                System.out.println(cnse.getStackTrace());
+//            }
+//        });
+//
+////        Print Results
+//        for (Circle c : theseCircles){
+//            System.out.println(c.toString());
+//        }
 
-        Collections.sort(j8Circles, (Circle o1, Circle o2) -> Double.compare(o1.getArea(), o2.getArea()));
 
 //        Parameters Type Free Comparator
 //        Comparator<Circle> customComparator = (c1, c2) -> Double.compare(c1.getArea(), c2.getArea());
@@ -76,10 +185,10 @@ public class Main {
 
 //        Lambda Expressions that to the same thing
 //        j8Circles.forEach(c -> System.out.println(c));
-        j8Circles.forEach(System.out::println);
+
 
 //        Java 8 - Map Reduce Filter Collect
-        System.out.println("\n\nJava 8 Streams: ");
+     //   System.out.println("\n\nJava 8 Streams: ");
 
 //        We want to know the average area of our circles
         double average = theseCircles
@@ -130,5 +239,11 @@ public class Main {
             return radius2 >= deltaX2 + deltaY2;
         })
         .forEach(c -> System.out.println("I " + c.getName() + " collided with " + collisionCircle.getName()));
+    }
+
+ //http://www.java2s.com/Tutorials/Java/Stream_How_to/IntStream/Use_lambda_to_check_if_a_integer_is_a_prime_number.htm
+    private static  boolean isPrime(long number) {
+        LongPredicate isDivisible = index -> number % index == 0;
+        return number > 1 && LongStream.range(2, number - 1).noneMatch(isDivisible);
     }
 }
