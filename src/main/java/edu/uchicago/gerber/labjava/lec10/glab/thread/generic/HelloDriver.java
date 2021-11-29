4370f3882c8ce06311df6abd34273fb279961448
Adam Gerber <adam.gerber@yahoo.com>
2021-11-29 15:13:15 -0600

    prime numbers

--- src/main/java/edu/uchicago/gerber/labjava/lec10/glab/thread/generic/HelloDriver.java
+++ src/main/java/edu/uchicago/gerber/labjava/lec10/glab/thread/generic/HelloDriver.java
@@ -1,5 +1,8 @@
 package edu.uchicago.gerber.labjava.lec10.glab.thread.generic;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class HelloDriver {
 
 	/**
@@ -7,21 +10,53 @@ public class HelloDriver {
 	 */
 	public static void main(String[] args) {
 
+		boolean constrained = false;
 
-		SayHello sayHello = new SayHello("Hello World");
-		SayHello sayBye= new SayHello("Goodbye World");
-		
-//		sayHello.run();
-//		sayBye.run();
-		
-	
-		
-		Thread thr1 = new Thread(sayHello);
-		Thread thr2 = new Thread(sayBye);
-		
-		thr1.start();
-		thr2.start();
-		
+		SayHello one = new SayHello("1");
+		SayHello two = new SayHello("2");
+		SayHello three = new SayHello("3");
+		SayHello four = new SayHello("4");
+		SayHello five= new SayHello("5");
+		SayHello six= new SayHello("6");
+		SayHello seven = new SayHello("7");
+		SayHello eight = new SayHello("8");
+		SayHello nine = new SayHello("9");
+		SayHello line = new SayHello("#####################");
+
+
+	//	if (!constrained){
+
+			new Thread(one).start();
+			new Thread(two).start();
+			new Thread(three).start();
+			new Thread(four).start();
+			new Thread(five).start();
+			new Thread(six).start();
+			new Thread(seven).start();
+			new Thread(eight).start();
+			new Thread(nine).start();
+			new Thread(line).start();
+
+
+
+		//} else {
+//			ExecutorService executorService =	Executors.newFixedThreadPool(3);
+//			executorService.submit(one);
+//			executorService.submit(two);
+//			executorService.submit(three);
+//			executorService.submit(four);
+//			executorService.submit(five);
+//			executorService.submit(six);
+//			executorService.submit(seven);
+//			executorService.submit(eight);
+//			executorService.submit(nine);
+//			executorService.submit(line);
+		//}
+
+
+
+
+
 		
 		
 
--- src/main/java/edu/uchicago/gerber/labjava/lec10/glab/thread/generic/PrimeNumber.java
+++ src/main/java/edu/uchicago/gerber/labjava/lec10/glab/thread/generic/PrimeNumber.java
@@ -1,17 +1,15 @@
 package edu.uchicago.gerber.labjava.lec10.glab.thread.generic;
 
-import java.util.Date;
+public class PrimeNumber implements Runnable {
 
-public class SayHello implements Runnable {
-	
 	private static final int REPS = 10;
 	private static final int DELAY = 1000;
-	
-	
+
+
 	private String strGreet;
 
 
-	public SayHello(String strGreet) {
+	public PrimeNumber(String strGreet) {
 		super();
 		this.strGreet = strGreet;
 	}
@@ -29,10 +27,14 @@ public class SayHello implements Runnable {
 
 	@Override
 	public void run() {
-		
+
+
 		try {
 			for (int nC = 0; nC < REPS; nC++) {
 				long start = System.currentTimeMillis();
+				for (int nD = 0; nD < 50_000; nD++){
+					calcPrime(nD);
+				}
 
 				Thread.sleep(DELAY);
 				System.out.println((System.currentTimeMillis() - start) + " " + strGreet);
@@ -42,6 +44,16 @@ public class SayHello implements Runnable {
 		   	//exp.printStackTrace();
 		}
 		
+	}
+
+	private boolean calcPrime(int num){
+		for (int i = 2; i <= num / 2; ++i) {
+			// condition for nonprime number
+			if (num % i == 0) {
+				return true;
+			}
+		}
+		return false;
 	}
 	
 	
