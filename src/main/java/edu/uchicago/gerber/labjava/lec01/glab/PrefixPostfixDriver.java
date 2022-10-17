254b7c0dfe5f1127168fe13a5f476a3f4a1b3e4c
Adam Gerber <adam.gerber@yahoo.com>
2022-10-17 15:32:09 -0500

    PrefixPostfixDriver

---
+++ src/main/java/edu/uchicago/gerber/labjava/lec01/glab/PrefixPostfixDriver.java
@@ -0,0 +1,20 @@
+package edu.uchicago.gerber.labjava.lec01.glab;
+
+public class PrefixPostfixDriver {
+    public static void main(String[] args) {
+
+        int nX = 0;
+
+       // prefix ->
+        // nX = nX + 1;
+        System.out.println(++nX);
+        // postfix ->
+        // nX = nX + 1;
+
+
+
+
+
+
+    }
+}
