41ce889363566357e40de5e495a68251fd27a436
Adam Gerber <adam.gerber@yahoo.com>
2023-10-01 13:38:51 -0500

    subthread may override

--- src/main/java/edu/uchicago/gerber/labjava/lec10/glab/_01_basics/create/_01_ExtendThreadDriver.java
+++ src/main/java/edu/uchicago/gerber/labjava/lec10/glab/_01_basics/create/_01_ExtendThreadDriver.java
@@ -5,8 +5,8 @@ package edu.uchicago.gerber.labjava.lec10.glab._01_basics.create;
 public class _01_ExtendThreadDriver {
     public static void main(String[] args) {
         MyThread thread = new MyThread();
-        thread.setPriority(Thread.MAX_PRIORITY); //optional
-        thread.setName("MyThread"); // optional - set thread name
+        //thread.setPriority(Thread.MAX_PRIORITY); //optional
+        //thread.setName("MyThread"); // optional - set thread name
         thread.start(); // Start the thread
     }
 }
@@ -14,7 +14,7 @@ public class _01_ExtendThreadDriver {
 class MyThread extends Thread {
     @Override
     public void run() {
-        // Thread's logic goes here
+        // Thread's running logic goes here. Anything in this method will happen on the MyThread thread.
         System.out.println("I'm using the ORIGINAL ENGINE of this thread...");
     }
 }
