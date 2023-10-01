75701d761ed5c7e74e68a129af7f77f38557edc1
Adam Gerber <adam.gerber@yahoo.com>
2023-10-01 10:39:35 -0500

    more comments

--- src/main/java/edu/uchicago/gerber/labjava/lec01/glab/PrefixPostfixDriver.java
+++ src/main/java/edu/uchicago/gerber/labjava/lec01/glab/PrefixPostfixDriver.java
@@ -3,13 +3,20 @@ package edu.uchicago.gerber.labjava.lec01.glab;
 public class PrefixPostfixDriver {
     public static void main(String[] args) {
 
-        int nX = 0;
+        //postfix
+        int numPost = 0;
+        System.out.println(numPost++);
+        //numPost is incremented after the expression above is evaluated
+        System.out.println(numPost);
 
-       // prefix ->
-        // nX = nX + 1;
-        System.out.println(nX++);
-        // postfix ->
-        // nX = nX + 1;
+        System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
+        System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
+
+        //prefix
+        int numPre = 0;
+        System.out.println(++numPre); //numPre is incremented then the expression is evaluated
+        System.out.println(numPre);
+
 
 
 
