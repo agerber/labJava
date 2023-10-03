280172a93bbfe9427e9b78a54401d0e80bb57c59
Adam Gerber <adam.gerber@yahoo.com>
2023-10-03 17:58:11 -0500

    dlocks

--- src/main/java/edu/uchicago/gerber/labjava/lec10/glab/_04_deadlock/NoDeadLockExample.java
+++ src/main/java/edu/uchicago/gerber/labjava/lec10/glab/_04_deadlock/NoDeadLockExample.java
@@ -2,33 +2,33 @@ package edu.uchicago.gerber.labjava.lec10.glab._04_deadlock;
 
 public class NoDeadLockExample {
 
-    private static final Object lock1 = new Object();
-    private static final Object lock2 = new Object();
+    private static final Object lockA = new Object();
+    private static final Object lockB = new Object();
 
     public static void main(String[] args) {
         Thread thread1 = new Thread(() -> {
-            synchronized (lock1) {
+            synchronized (lockA) {
                 System.out.println("Thread1 acquired lock1");
 
                 try {
                     Thread.sleep(100);
                 } catch (InterruptedException e) {}
 
-                synchronized (lock2) {
+                synchronized (lockB) {
                     System.out.println("Thread1 acquired lock2");
                 }
             }
         });
 
         Thread thread2 = new Thread(() -> {
-            synchronized (lock1) {
+            synchronized (lockA) {
                 System.out.println("Thread2 acquired lock1");
 
                 try {
                     Thread.sleep(100);
                 } catch (InterruptedException e) {}
 
-                synchronized (lock2) {
+                synchronized (lockB) {
                     System.out.println("Thread2 acquired lock2");
                 }
             }
