45e93912b18766c32f79dd93fe2cd731780f161f
Adam Gerber <adam.gerber@yahoo.com>
2023-10-06 13:09:23 -0500

    set prioty

--- src/main/java/edu/uchicago/gerber/labjava/lec10/glab/_01_basics/create/ExtendThreadDriver.java
+++ src/main/java/edu/uchicago/gerber/labjava/lec10/glab/_01_basics/create/ExtendThreadDriver.java
@@ -5,7 +5,7 @@ package edu.uchicago.gerber.labjava.lec10.glab._01_basics.create;
 public class ExtendThreadDriver {
     public static void main(String[] args) {
         MyThread thread = new MyThread();
-        //thread.setPriority(Thread.MAX_PRIORITY); //optional
+        thread.setPriority(Thread.MIN_PRIORITY); //optional
         //thread.setName("MyThread"); // optional - set thread name
         thread.start(); // Start the thread
     }
