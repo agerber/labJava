ed49352f846d0c9259d69b0dae1e44b5f9d25c8c
Adam Gerber <adam.gerber@yahoo.com>
2023-10-08 16:06:11 -0500

    with threadPool

---
+++ src/main/java/edu/uchicago/gerber/labjava/lec10/glab/_03_util_concurrent/data_structs/ProducerConsumerBlockingQueue.java
@@ -0,0 +1,50 @@
+package edu.uchicago.gerber.labjava.lec10.glab._03_util_concurrent.data_structs;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+public class ProducerConsumerBlockingQueue {
+
+    /*
+    Because the consumer is slower than the producer, the queue will eventually fill up, and the producer will block waiting for space to become available in the queue.
+     */
+    public static void main(String[] args) {
+        BlockingQueue<String> blockingQueue = new ArrayBlockingQueue<>(7);
+
+        //producer is 3x faster than consumer, so blocking queue will fill up
+        Thread producerThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 50; i++) {
+                    try {
+                        //put is blocking
+                        blockingQueue.put("Item " + i);
+                        System.out.println("Produced item " + i);
+                        Thread.sleep(50);  // simulating some time taken to produce an item
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        Thread consumerThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 150; i++) {
+                    try {
+                        //take() is blocking
+                        String item = blockingQueue.take();
+                        System.out.println("Consumed " + item);
+                        Thread.sleep(150);  // simulating some time taken to consume an item
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        producerThread.start();
+        consumerThread.start();
+    }
+}
