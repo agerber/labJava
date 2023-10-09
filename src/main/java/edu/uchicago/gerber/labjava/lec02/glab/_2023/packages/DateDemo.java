d8f47d4051347622786c05b8a3e2b4ff98fc2045
Adam Gerber <adam.gerber@yahoo.com>
2023-10-09 13:35:47 -0500

    more

---
+++ src/main/java/edu/uchicago/gerber/labjava/lec02/glab/_2023/packages/DateDemo.java
@@ -0,0 +1,17 @@
+package edu.uchicago.gerber.labjava.lec02.glab._2023.packages;
+
+// Importing Date from java.util package
+import java.util.Date;
+
+public class DateDemo {
+
+    public static void main(String[] args) {
+        // Using Date from java.util package
+        Date utilDate = new Date();
+        System.out.println("java.util.Date: " + utilDate);
+
+        // Using Date from java.sql package using fully-qualified name
+        java.sql.Date sqlDate = new java.sql.Date(utilDate.getTime());
+        System.out.println("java.sql.Date: " + sqlDate);
+    }
+}
