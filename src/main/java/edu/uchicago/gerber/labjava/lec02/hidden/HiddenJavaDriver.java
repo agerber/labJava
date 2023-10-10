c47656600d5a21ae62bf6042dd3abf6f953f0bd4
Adam Gerber <adam.gerber@yahoo.com>
2023-10-10 15:52:52 -0500

    hidden java

--- src/main/java/edu/uchicago/gerber/labjava/lec02/hidden/HiddenJavaDriver.java
+++ src/main/java/edu/uchicago/gerber/labjava/lec02/hidden/HiddenJavaDriver.java
@@ -1,17 +1,35 @@
 package edu.uchicago.gerber.labjava.lec02.hidden;
-
+import java.lang.*;
+import java.awt.Rectangle;
 public class HiddenJavaDriver {
 
-    /*
-    Hidden Java:
-import java.lang.*;
-extends Object / Other
-no-arg constructor
 
-     */
+    //   HIDDEN JAVA
+    //1. by default the java.lang.* package is imported for you. The import java.lang.* above is redundant.
+    //2. If you do not provide a constructor in your class (see below), a default no-arg constructor is provided to you.
+         //Upon construction, all fields will be set to their default values, so intValue will be set to 0, dubValue
+         // will be set to 0.0, and rectangle will be set to null.
+    //3. All classes in Java "extends Object" by default, even Drivers and utility classes.  The 'extends Object' below
+         //is totally redundant.
+
 
     public static void main(String[] args) {
-        HiddenJavaDriver hiddenJava = new HiddenJavaDriver();
+        System.out.println(new MySimple());
     }
 
+}
+
+class MySimple extends Object {
+    private int intValue;
+    private double dubValue;
+    private Rectangle rectangle;
+
+    @Override
+    public String toString() {
+        return "MySimple{" +
+                "intValue=" + intValue +
+                ", dubValue=" + dubValue +
+                ", rectangle=" + rectangle +
+                '}';
+    }
 }
