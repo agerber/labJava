ce74527e31702d4c4fb862fc85e2249a063d3ae7
Adam Gerber <adam.gerber@yahoo.com>
2023-10-10 11:15:07 -0500

    false swap with mutations

--- src/main/java/edu/uchicago/gerber/labjava/lec02/refs/FalseSwapDemo.java
+++ src/main/java/edu/uchicago/gerber/labjava/lec02/refs/FalseSwapDemo.java
@@ -6,18 +6,78 @@ public class FalseSwapDemo {
         Person personA = new Person("Alice");
         Person personB = new Person("Bob");
 
-        System.out.println("Before swap:");
+        System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
+
+        System.out.println("Before falseSwap:");
         System.out.println("personA: " + personA);
         System.out.println("personB: " + personB);
 
         falseSwap(personA, personB);
 
-        System.out.println("\nAfter swap:");
+        System.out.println("\nAfter falseSwap:");
         System.out.println("personA: " + personA);
         System.out.println("personB: " + personB);
+
+        System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
+        System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
+
+        System.out.println("Before mutateThenFalseSwap:");
+        System.out.println("personA: " + personA);
+        System.out.println("personB: " + personB);
+
+        mutateThenFalseSwap(personA, personB);
+
+        System.out.println("\nAfter mutateThenFalseSwap:");
+        System.out.println("personA: " + personA);
+        System.out.println("personB: " + personB);
+
+        System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
+        System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
+
+        System.out.println("Before mutateObjects:");
+        System.out.println("personA: " + personA);
+        System.out.println("personB: " + personB);
+
+        mutateObjects(personA, personB);
+
+        System.out.println("\nAfter mutateObjects:");
+        System.out.println("personA: " + personA);
+        System.out.println("personB: " + personB);
+        System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
+
     }
 
     public static void falseSwap(Person a, Person b) {
+
+        //because these references (memory addresses) are copies, however, if we attempt to repoint the
+        //local references, these changes are NOT reflected outside the scope of this method.
+        Person temp = a;
+        a = b;
+        b = temp;
+
+        // Let's print inside the method after the swap
+        System.out.println("\nInside method after swap:");
+        System.out.println("a: " + a);
+        System.out.println("b: " + b);
+    }
+
+    public static void mutateObjects(Person a, Person b){
+        //When you pass an object reference (copy of memory address) into a method, the method
+        //can still MUTATE the objects
+        a.setName(a.getName() + "-|MUTATED|-");
+        b.setName(b.getName() + "-|MUTATED|-");
+
+    }
+
+    public static void mutateThenFalseSwap(Person a, Person b) {
+
+        //When you pass an object reference (copy of memory address) into a method, the method
+        //can still MUTATE the objects
+        a.setName(a.getName() + "-|MUTATED|-");
+        b.setName(b.getName() + "-|MUTATED|-");
+
+        //because these references (memory addresses) are copies, however, if we attempt to repoint the
+        //local references, these changes are NOT reflected outside the scope of this method.
         Person temp = a;
         a = b;
         b = temp;
@@ -40,4 +100,16 @@ class Person {
     public String toString() {
         return name;
     }
+
+    //getter (accessor)
+    public String getName() {
+        return name;
+    }
+
+    //setter (mutator)
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
 }
