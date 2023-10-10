256e29e19abf8771a3bbaaa7b013adb695a6d9e6
Adam Gerber <adam.gerber@yahoo.com>
2023-10-10 09:37:18 -0500

    expolanation of staticVsInstance methods

--- src/main/java/edu/uchicago/gerber/labjava/lec02/statics/InstanceVsStaticMethodsDriver.java
+++ src/main/java/edu/uchicago/gerber/labjava/lec02/statics/InstanceVsStaticMethodsDriver.java
@@ -1,13 +1,28 @@
 package edu.uchicago.gerber.labjava.lec02.statics;
 
+
+
+
 public class InstanceVsStaticMethodsDriver {
 
+    /*
+    Both instance-method-definitions and static-method-definitions are stored in the
+    class-object and loaded by the class-loader before runtime. However, all
+    instances (objects) are created at runtime.
+     */
+
+
+    //my instance field
     private int value;
 
+    //this method can be either instance or static as it does not reference any
+    //instance fields
     public static int canBeInstanceOrStatic(int n){
         return n + 10;
     }
 
+    //this method MUST be an instance method because it references a field.
+    //if you try to make it static, the compiler will complain.
     public int mustBeInstance(){
         return value;
 
