303d1dbaf3b048e7c21c95c911d5ad8f2d970dd9
Adam Gerber <adam.gerber@yahoo.com>
2023-10-11 09:25:30 -0500

    more comments

--- src/main/java/edu/uchicago/gerber/labjava/lec02/refs/AnonObjectsDemo.java
+++ src/main/java/edu/uchicago/gerber/labjava/lec02/refs/AnonObjectsDemo.java
@@ -10,7 +10,6 @@ public class AnonObjectsDemo {
         // Define size for demonstration
         int size = 5;
 
-       Rectangle rec =  new Rectangle(1,2,3,4);
         // Populate an array with anonymous Rectangle objects
         Rectangle[] rectanglesArray = new Rectangle[size];
         for (int i = 0; i < size; i++) {
@@ -24,6 +23,10 @@ public class AnonObjectsDemo {
             rectanglesList.add(new Rectangle(i, i, i + 2, i + 2));
             System.out.println("LinkedList Rectangle " + i + ": " + rectanglesList.get(i));
         }
+
+        //create an object and store it in a reference, then add it to the list.
+        //this is the counter-example to above where we are passing reference-anonymous objects
+        Rectangle rec =  new Rectangle(1,2,3,4);
         rectanglesList.add(rec);
     }
 }
--- src/main/java/edu/uchicago/gerber/labjava/lec02/refs/PassByDriver.java
+++ src/main/java/edu/uchicago/gerber/labjava/lec02/refs/PassByDriver.java
@@ -4,12 +4,16 @@ public class PassByDriver {
 
     public static void main(String[] args) {
         // Primitive
+        //when we pass a primitive, we pass a copy of the value. The original value outside
+        //the scope of this method will be unaffected
         int primitiveValue = 10;
         System.out.println("Before passing primitive: " + primitiveValue);
         modifyPrimitive(primitiveValue);
         System.out.println("After passing primitive: " + primitiveValue);
 
         // Object
+        //when we pass an object, we pass a copy of the value of the memory address.
+        //once the method has the address, it CAN mutate the object.
         IntegerWrapper objectValue = new IntegerWrapper(10);
         System.out.println("\nBefore passing object: " + objectValue.getValue());
         modifyObject(objectValue);
