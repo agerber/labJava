d97d27f00b450cc5499945737316b7976de451a0
Adam Gerber <adam.gerber@yahoo.com>
2023-10-12 17:02:57 -0500

    god views

--- src/main/java/edu/uchicago/gerber/labjava/lec03/simplepoly/DubToObjectGodView.java
+++ src/main/java/edu/uchicago/gerber/labjava/lec03/simplepoly/DubToObjectGodView.java
@@ -7,31 +7,31 @@ public class DubToObjectGodView {
         Double dub =34.56;
         //System.out.println(dub.getClass().getName());
         GodView.print(Double.class, GodView.Size.LARGE, false);
-        GodView.print(Double.class, GodView.Size.LARGE, true);
+        GodView.print(dub.getClass(), GodView.Size.LARGE, true);
 
 
         //up-cast (automatic casting)
         Number num = (Number) dub;
         GodView.print(Number.class, GodView.Size.MEDIUM, false);
-        GodView.print(Double.class, GodView.Size.LARGE, true);
+        GodView.print(num.getClass(), GodView.Size.LARGE, true);
 
 
         //up-cast (automatic casting)
         Object obj = (Object) num;
         GodView.print(Object.class, GodView.Size.SMALL, false);
-        GodView.print(Double.class, GodView.Size.LARGE, true);
+        GodView.print(obj.getClass(), GodView.Size.LARGE, true);
 
 
         //down-casting (widening)
         Double anotherDub = (Double) obj;
         GodView.print(Double.class, GodView.Size.LARGE, false);
-        GodView.print(Double.class, GodView.Size.LARGE, true);
+        GodView.print(anotherDub.getClass(), GodView.Size.LARGE, true);
 
 
         //up-cast (automatic casting)
         Number anotherNum = (Number) anotherDub;
         GodView.print(Number.class, GodView.Size.MEDIUM, false);
-        GodView.print(Double.class, GodView.Size.LARGE, true);
+        GodView.print(anotherNum.getClass(), GodView.Size.LARGE, true);
 
         //how many objects are instantiated in this example?
 
