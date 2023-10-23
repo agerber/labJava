08d795deee43a012e1bd7623d5aeaeeed0506057
Adam Gerber <adam.gerber@yahoo.com>
2023-10-23 10:26:43 -0500

    program to inter

---
+++ src/main/java/edu/uchicago/gerber/labjava/lec04/prog_to_interfaces/Driver.java
@@ -0,0 +1,38 @@
+package edu.uchicago.gerber.labjava.lec04.prog_to_interfaces;
+
+import java.util.*;
+
+public class Driver {
+
+
+
+    public static void main(String[] args) {
+
+        Map<String,String> map = new HashMap<>();
+
+        map.put("United States", "North America");
+        map.put("Lithuania", "Europe");
+        map.put("Chile", "South America");
+        map.put("China", "Asia");
+        map.put("Denmark", "Europe");
+        map.put("Vietnam", "Asia");
+
+        printMap(map);
+
+
+    }
+
+
+    private static void printMap(Map<String,String> map){
+
+        for (String key : map.keySet()) {
+            System.out.println(getCountryAndContinent(key, map));
+        }
+    }
+
+    private static String getCountryAndContinent(String key, Map<String,String> map){
+        return key + " : " + map.get(key);
+    }
+
+
+}
