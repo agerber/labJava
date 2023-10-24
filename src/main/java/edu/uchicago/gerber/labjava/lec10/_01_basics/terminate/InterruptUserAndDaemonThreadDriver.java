d9c1ff8860727307410146a52c553dd16e70cd2c
Adam Gerber <adam.gerber@yahoo.com>
2023-10-24 11:39:27 -0500

    terminating

--- src/main/java/edu/uchicago/gerber/labjava/lec10/_01_basics/terminate/InterruptUserAndDaemonThreadDriver.java
+++ src/main/java/edu/uchicago/gerber/labjava/lec10/_01_basics/terminate/InterruptUserAndDaemonThreadDriver.java
@@ -4,6 +4,28 @@ import java.util.Random;
 
 public class InterruptUserAndDaemonThreadDriver {
 
+    /*
+    Scenario 1: Rogue child User-Thread
+    //child.setDaemon(true);
+    //child.interrupt();
+
+     Scenario 2: child User-Thread interrupted (you must remember to call interrupt)
+    //child.setDaemon(true);
+    child.interrupt();
+
+    Scenario 3: child Daemon-Thread terminates at the end of the lifecycle of its parent-Thread
+    child.setDaemon(true);
+    //child.interrupt(); //no need to interrupt
+
+
+    Scenario 4: child Daemon-Thread terminates at the end of the lifecycle of its parent-Thread
+    child.setDaemon(true);
+    child.interrupt(); //this is ignored :(
+
+
+     */
+
+
     public static void main(String[] args) {
         Thread child = new Thread(new Runnable() {
             private final Random random = new Random();
@@ -11,6 +33,7 @@ public class InterruptUserAndDaemonThreadDriver {
             //some long-running cpu-intensive operation
             @Override
             public void run() {
+                //this is an infinite loop!
                 while (true) {
                     if (!Thread.interrupted()) {
                         long num = random.nextLong();
@@ -27,17 +50,17 @@ public class InterruptUserAndDaemonThreadDriver {
         });
 
 
-        child.setDaemon(true);
+        //child.setDaemon(true);
         child.start();
 
         // Let the main-thread sleep for one second
         try {
             Thread.sleep(1000);
+            //child.interrupt();
         } catch (InterruptedException e) {
-            System.out.println("main thread interrupted");
+            System.out.println(e.getMessage());
         }
 
-        //child.interrupt();
 
         System.out.println("Exiting main thread");
 
