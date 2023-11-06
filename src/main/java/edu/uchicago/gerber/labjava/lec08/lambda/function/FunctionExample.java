1b9827d3475e18969e4de1c450ef69bf0d0f5b84
Adam Gerber <adam.gerber@yahoo.com>
2023-11-05 21:49:04 -0600

    functions

--- src/main/java/edu/uchicago/gerber/labjava/lec08/lambda/function/FunctionExample.java
+++ src/main/java/edu/uchicago/gerber/labjava/lec08/lambda/function/FunctionExample.java
@@ -1,24 +1,38 @@
 package edu.uchicago.gerber.labjava.lec08.lambda.function;
 
 import java.util.function.Function;
+import java.util.Date;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class FunctionExample {
     public static void main(String[] args) {
-        Function<Integer, String> converter = (i) -> Integer.toString(i);
 
 
-        System.out.println("The result is " + converter.apply(3));
-        System.out.println("The result is " + converter.apply(30));
+        //takes an integer and returns a string
+        Function<Integer, String> converter = (x) -> Integer.toString(x);
+
+        //takes an integer and returns an integer
+        Function<Integer, Integer> doubler = (x) -> x * 2;
+
+        //takes a Date and returns a Long
+        Function<Date, Long>  epoch = (d) -> d.getTime();
 
 
-       // String result = calc((a) -> "Result: " + (a * 2), 10);
+        System.out.println("The conversions is " + converter.apply(30));
+        System.out.println("The double is " + doubler.apply(30));
+        System.out.println("The epoch is " + epoch.apply(new Date()));
 
-      //  System.out.println(result);
+
+        Stream.of(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+                .map(doubler)
+                .forEach(num -> System.out.println(num));
+
+
+
+
     }
 
-//    public static String calc(Function<Integer, String> bi, Integer i) {
-//        return bi.apply(i);
-//    }
 
 
 }
