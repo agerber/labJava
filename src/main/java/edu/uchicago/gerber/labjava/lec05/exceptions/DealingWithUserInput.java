f84b6f3efe07e319ed99126b023f832c365252dc
Adam Gerber <adam.gerber@yahoo.com>
2023-11-05 20:08:58 -0600

    user input

---
+++ src/main/java/edu/uchicago/gerber/labjava/lec05/exceptions/DealingWithUserInput.java
@@ -0,0 +1,29 @@
+package edu.uchicago.gerber.labjava.lec05.exceptions;
+
+import java.util.Scanner;
+
+public class DealingWithUserInput {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int num;
+
+        while (true){
+            System.out.println("Enter your integer:");
+            try {
+                num =  Integer.parseInt(in.nextLine());
+                break;
+            } catch (NumberFormatException e) {
+                System.out.print("Sorry, try again. ");
+            }
+        }
+
+        System.out.println("Well done, " + num + " is an integer." );
+
+
+
+
+
+    }
+}
