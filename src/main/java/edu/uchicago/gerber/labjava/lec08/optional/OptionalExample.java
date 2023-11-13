99bd25920c0ccb2911c7b308906003a3fc9528d3
Adam Gerber <adam.gerber@yahoo.com>
2023-11-13 14:17:05 -0600

    Consumer

--- src/main/java/edu/uchicago/gerber/labjava/lec08/optional/OptionalExample.java
+++ src/main/java/edu/uchicago/gerber/labjava/lec08/optional/OptionalExample.java
@@ -22,22 +22,25 @@ public class OptionalExample {
 
         // Creating an Optional object that might be null
         String nullableString = getNullableString();
-        Optional<String> optionalNullable = Optional.ofNullable(nullableString);
+        Optional<String> optional = Optional.ofNullable(nullableString);
 
-        // Using ifPresent() to provide a lambda expression to execute if a value is present
-        optionalNullable.ifPresent(str -> System.out.println(str));
+        //isPresent() returns a boolean
+        System.out.println(optional.isPresent());
+
+        // Using ifPresent(Consumer consumer) to provide a lambda expression to execute if a value is present
+        optional.ifPresent(str -> System.out.println(str));
 
         // Using orElse() to retrieve the value if present, or get a default one if not
-        String valueOrDefault = optionalNullable.orElse("Default Value from orElse");
+        String valueOrDefault = optional.orElse("Default Value from orElse");
         System.out.println(valueOrDefault);
 
         // Using orElseGet() to retrieve the value or get it from a supplier functional interface
-        String valueOrGetDefault = optionalNullable.orElseGet(() -> "Default Value from Supplier");
+        String valueOrGetDefault = optional.orElseGet(() -> "Default Value from Supplier");
         System.out.println(valueOrGetDefault);
 
         // Using orElseThrow() to throw an exception if the Optional is empty
         try {
-            String valueOrThrow = optionalNullable.orElseThrow(() -> new IllegalArgumentException("No value present!"));
+            String valueOrThrow = optional.orElseThrow(() -> new IllegalArgumentException("No value present!"));
             System.out.println(valueOrThrow);
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
